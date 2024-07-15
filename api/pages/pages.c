267c44627f8dc3260917b1d6c14522b911573e48
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2024-07-15 16:45:08 +0530

    Increase name and style size

--- api/pages/pages.c
+++ api/pages/pages.c
@@ -16,17 +16,17 @@ void create_pages() {
     while (token != NULL) {
         char* find_str = strdup(token);
         if (strstr(find_str, "=") != NULL) {
-            char* name = (char*)malloc(100);
-            char* style = (char*)malloc(100);
+            char* name = (char*)malloc(1000);
+            char* style = (char*)malloc(1000);
             char* content = (char*)malloc(1000000);
             name[0] = '\0';
             content[0] = '\0';
 
             int name_size = extract_variable_name(name, find_str);
-            name = realloc(name, name_size);
+            name = realloc(name, name_size + 1);
 
             int style_size = extract_section_name(style, find_str);
-            style = realloc(style, style_size);
+            style = realloc(style, style_size + 1);
 
             outer_saveptr = content_between_braces(content, outer_saveptr, "{", "}");
             struct Page* page = (struct Page*)malloc(name_size + style_size + strlen(content));
